c01639f72648445e2ded406774486567a79952b4
TheShinesoul <jimcont.hector@gmail.com>
2021-03-07 15:29:49 -0600

    add configurations

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import HelloWord from './components/HelloWord'
+
+
+ReactDom.render(
+    <HelloWord/>, document.getElementById('app')
+)
